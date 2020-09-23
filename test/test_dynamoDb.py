e2bec48999e005939590884beacd87d2ded441b5
malix1 <mali.caferx@gmail.com>
2020-09-23 09:57:09 +0900

    removed unnecessary variable

--- test/test_dynamoDb.py
+++ test/test_dynamoDb.py
@@ -12,13 +12,13 @@ def test_save_book(dynamoDb):
 
 def test_remove_book(dynamoDb):
     book = {"name": "lotr", "type": "type", "BookID": "123"}
-    response = dynamoDb.save_book(book)
+    dynamoDb.save_book(book)
     response = dynamoDb.remove_book(book)
     assert response['ResponseMetadata']['HTTPStatusCode'] == 200
 
 
 def test_get_book(dynamoDb):
     book = {"name": "lotr", "type": "type", "BookID": "123"}
-    response1 = dynamoDb.save_book(book)
+    dynamoDb.save_book(book)
     response = dynamoDb.get_book(book)
     assert response['ResponseMetadata']['HTTPStatusCode'] == 200
