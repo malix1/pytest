7897363f42c33037cbba0b249af217ee345c871a
malix1 <mali.caferx@gmail.com>
2020-09-23 09:31:50 +0900

    changed name of book

--- test/test_dynamoDb.py
+++ test/test_dynamoDb.py
@@ -5,20 +5,20 @@ from Databases.DynamoDb import Db
 
 
 def test_save_book(dynamoDb):
-    book = {"name": "asd", "type": "type"}
+    book = {"name": "lotr", "type": "type"}
     response = dynamoDb.save_book(book)
     assert response['ResponseMetadata']['HTTPStatusCode'] == 200
 
 
 def test_remove_book(dynamoDb):
-    book = {"name": "asd", "type": "type", "BookID": "123"}
+    book = {"name": "lotr", "type": "type", "BookID": "123"}
     response = dynamoDb.save_book(book)
     response = dynamoDb.remove_book(book)
     assert response['ResponseMetadata']['HTTPStatusCode'] == 200
 
 
 def test_get_book(dynamoDb):
-    book = {"name": "asd", "type": "type", "BookID": "123"}
+    book = {"name": "lotr", "type": "type", "BookID": "123"}
     response1 = dynamoDb.save_book(book)
     response = dynamoDb.get_book(book)
     assert response['ResponseMetadata']['HTTPStatusCode'] == 200
